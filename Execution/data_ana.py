99645ffc07cdc8efb6e09dd9cd72750af0b8034f
H.Guertner <h.guertner@physik.uni-muenchen.de>
2015-12-20 17:32:35 +0100

    Scripts to Run Program

---
+++ Execution/data_ana.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib
+# Force matplotlib to not use any Xwindows backend.
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import os, errno, sys
+import math
+import json
+from scipy.optimize import curve_fit
+
+
+# import parameters
+with open('DiagMC_BEC.json') as params_file:
+    params = json.load(params_file)
+
+# data for the comparison    
+peter_loc = params["Peter_Path"]
+if params["Froehlich_Polaron"]:
+	if params["Self_Energy" ] and params["Max_Order"] == 6:
+		compare = ["FP_1","FP_2","FP_SE_6ord"]
+	else:
+		compare = ["FP_1","FP_2","FP_all"]
+	scale = 'linear'
+else:
+	compare = ["mat_1st_g0seg0", "mat_2nd_g0se","fig3total"]
+	scale= 'log'
+    
+# cleanup
+def silentremove(filename):
+    try:
+        os.remove(filename)
+    except OSError as e:
+        if e.errno != errno.ENOENT: # errno.ENOENT = no such file or directory
+            raise # re-raise exception if a different error occured
+		  
+silentremove('ana/plot_0.pdf')
+silentremove('ana/plot_all.pdf')
+silentremove('ana/plot_1.pdf')
+silentremove('ana/plot_2a.pdf')
+silentremove('ana/plot_2b.pdf')
+silentremove('ana/control/plot_first_Norm_End.pdf')
+silentremove('ana/control/plot_last_Norm_End.pdf')
+
+
+# definitions
+pi = math.pi
+
+# plot fake-function
+#sample
+fake = np.loadtxt('data/zero_order')
+plt.errorbar(fake[:, 0], fake[:, 1], fake[:, 2], label='Sampled')
+
+# analytical
+def fake_integrand(tau):
+  disp = np.power(params['Momentum'], 2)/2. - params['Chemical_Potential']
+  return np.exp(- disp * tau)
+
+fake_integrand = np.vectorize(fake_integrand)
+plt.plot(fake[:, 0], fake_integrand(fake[:, 0]), label='Analytical result')
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'Fake-function')
+plt.xlim([0, params['Tau_max']])
+plt.legend()
+plt.savefig('ana/plot_0.pdf')
+
+plt.clf()
+
+# plot Green-function
+#def mucor(tau):
+#  return np.exp(params['Chemical_Potential'] * tau)
+#mucor = np.vectorize(mucor)
+
+#sample
+data = np.loadtxt('data/all_orders')
+plt.errorbar(data[:, 0], data[:, 1], data[:, 2], label='Sampled')
+
+# Peter
+peter = np.loadtxt(peter_loc+'/'+compare[2])
+plt.plot(peter[:, 0], peter[:, 1], label=compare[2])
+
+#fitting data
+def Gtaugrwp(tau, Z, Ek):
+	return Z*np.exp(-(Ek-params["Chemical_Potential"])*tau)
+
+#write fit file
+#popt, pcov = curve_fit(Gtaugrwp, data[50:,0], data[50:,1])
+#perr = np.sqrt(np.diag(pcov)) 
+#fitFile = open("ana/fitparams", "w")
+#fitFile.write("#Fit Parameters \n")
+#fitFile.write("Polaron Energy \n")
+#fitFile.write("E = "+str(popt[1])+" +- "+ str(perr[1])+"\n \n")
+#fitFile.write("Z = "+str(popt[0])+" +- "+ str(perr[0]))
+#fitFile.close()
+
+#fitcurve = np.vectorize(Gtaugrwp)
+#plt.plot(data[:, 0], Gtaugrwp(data[:,0], popt[0], popt[1]), label="Fit alpha="+str(params["Coupling_Strength"])+" E=%.2f" %(popt[1]))
+plt.yscale(scale)
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'$\Sigma (\tau) e^{\mu \tau}$')
+plt.xlim([0, params['Tau_max']])
+plt.legend()
+plt.savefig('ana/plot_all.pdf')
+
+plt.clf()
+
+# plot 1st order
+#sample
+data = np.loadtxt('data/first_order')
+plt.errorbar(data[:, 0], data[:, 1], data[:, 2], label='Sampled')
+
+# Compare
+peter = np.loadtxt(peter_loc+ '/'+compare[0])
+plt.plot(peter[:, 0], peter[:, 1], label=compare[0])
+plt.yscale(scale)
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'$G_0 \Sigma G_0 (0, \tau)$')
+plt.xlim([0, params['Tau_max']])
+plt.legend()
+plt.savefig('ana/plot_1.pdf')
+
+plt.clf()
+
+# plot 2nd order a
+#sample
+data = np.loadtxt('data/second_ordera')
+plt.errorbar(data[:, 0], data[:, 1], data[:, 2], label='Sampled 2a')
+data2 = np.loadtxt('data/second_orderb')
+plt.errorbar(data2[:, 0], data2[:, 1], data2[:, 2], label='Sampled 2b')
+
+# Compare
+peter = np.loadtxt(peter_loc+ '/'+ compare[1])
+plt.plot(peter[:, 0], peter[:, 1], label=compare[1])
+plt.yscale(scale)
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'second order a')
+plt.xlim([0, params['Tau_max']])
+plt.legend()
+plt.savefig('ana/plot_2a.pdf')
+
+plt.clf()
+
+# plot 2nd order b
+#sample
+data = np.loadtxt('data/second_orderb')
+plt.errorbar(data[:, 0], data[:, 1], data[:, 2], label='Sampled 2b')
+
+# Compare
+plt.yscale(scale)
+plt.xlabel(r'$\tau$')
+plt.ylabel(r'second order b')
+plt.xlim([0, params['Tau_max']])
+plt.legend()
+plt.savefig('ana/plot_2b.pdf')
+plt.clf()
+
+
+#SECUMUL
+if params["SECumul"] and params["Self_Energy"]:
+	minmax= np.loadtxt('data/stats/minmax', skiprows = 3)
+	
+	#Compare first Norm and End Diagram 
+	data = np.loadtxt('data/secumul/Norm_first')
+	tmp = "%i" %(minmax[1,0])
+	plt.plot(data[:, 0], data[:, 1], label=('Norm Diagram Order '+tmp))
+	data2 = np.loadtxt('data/secumul/End_first')
+	tmp = "%i" %(minmax[0,2])
+	plt.plot(data2[:, 0], data2[:, 1], label='End Diagram Order ' +tmp)
+
+	plt.yscale(scale)
+	plt.xlabel(r'$\tau$')
+	plt.ylabel(r'Order '+tmp)
+	plt.xlim([0, params['Tau_max']])
+	plt.legend()
+	plt.savefig('ana/control/plot_first_Norm_End.pdf')
+	plt.clf()
+	
+	#Compare last Norm and End Diagram 
+	data = np.loadtxt('data/secumul/Norm_last')
+	tmp = "%i" %(minmax[1,0])
+	plt.plot(data[:, 0], data[:, 1], label='Norm Diagram Order '+tmp)
+	data2 = np.loadtxt('data/secumul/End_last')
+	tmp = "%i" %(minmax[0,2])
+	plt.plot(data2[:, 0], data2[:, 1], label='End Diagram Order ' +tmp)
+
+	plt.yscale(scale)
+	plt.xlabel(r'$\tau$')
+	plt.ylabel(r'Order ' + tmp)
+	plt.xlim([0, params['Tau_max']])
+	plt.legend()
+	plt.savefig('ana/control/plot_last_Norm_End.pdf')
+	plt.clf()
+	
+	#FP Check of Selfeneergy
+if params["Self_Energy"]:
+	if params["Froehlich_Polaron"]:
+		#silentremove('ana/plot_all.pdf')
+		
+		if params["FOG0SE"]:
+			os.system("math -script FP_g0SE_Transform.m")
+		else:
+			os.system("math -script FP_g0SEg0_Transform.m")
+		
+		#new all orders Green Function
+		data = np.loadtxt('data/FP_control/mat_FP_all_transform')
+		#interpoldata = np.loadtxt('data/FP_control/mat_FP_all_transform_interpol')
+		plt.plot(data[:, 0], data[:, 1], label='FP of SE Sampling Simpson')
+		#plt.plot(interpoldata[:, 0], interpoldata[:, 1], label='FP of SE Sampling Interpolation ')
+		peter = np.loadtxt(peter_loc+'/'+compare[2])
+		plt.plot(peter[:, 0], peter[:, 1], label=compare[2])
+	
+		plt.yscale(scale)
+		plt.xlabel(r'$\tau$')
+		plt.ylabel(r'G($\tau$)')
+		plt.xlim([0, params['Tau_max']])
+		plt.legend()
+		plt.savefig('ana/control/plot_all.pdf')
+		plt.clf()
+
+	
+
+
+
---
+++ Execution/submitjob.py
@@ -0,0 +1,268 @@
+#!/usr/bin/python3
+
+import os, glob, shutil
+import numpy as np
+import json
+
+# convenience function
+def change_dir(name):
+    if not os.path.exists(name):
+        os.makedirs(name)
+    os.chdir(name)
+
+#SUBMISSION SCRIPT
+#abbreviations for submission file (on ASC (sge) cluster)
+
+with open('DiagMC_BEC.json') as params_file:    
+    par = json.load(params_file)
+
+#Compile Options
+options = ""
+options2 = ""
+if par["Froehlich_Polaron"]:
+	options += "-DFP "
+if par["BEC_Polaron"]:
+	options += "-DBEC "
+#to split general DEFS and FP and BEC	
+if par["Self_Energy"]:
+	options2 += "-DSELFENERGY "
+if par["SECumul"]:
+	options2 += "-DSECUMUL "
+if par["MEASREWEIGHT"]:
+	options2 += "-DMEASREWEIGHT "
+if par["FOG0SE"]:
+	options2 += "-DFOG0SE "
+
+#recompile code
+os.chdir("src")
+if options != "":
+	with open('makefile', 'r') as makein:
+		maketext = makein.read().splitlines(True)
+	with open('makefile', 'w') as makeout:
+		makeout.write("DEFS = " + options+ options2+ "\n")
+		makeout.writelines(maketext[1:])
+ncores = par['NCores']
+os.system("export OMP_NUM_THREADS="+str(ncores))
+os.system("make clean")
+os.system("make")
+os.chdir(os.pardir)
+build_loc = os.getcwd()
+change_dir(par["Cluster"])
+execute_loc= os.getcwd()
+
+#submission file 
+name = "BEC"
+if par["SECumul"] and par["Self_Energy"]:
+	time_cluster = round(par['Total_Time']/60) + 3
+else:
+	time_cluster = round(par['RunTime']/60) + 3
+machine = par['Machine']
+constant_part = "#$ -l s_rt="+str(time_cluster)+":00:00\
+\n#$ -l h_vmem=" + str(2*(ncores+1)) + "00M\
+\n#$ -M h.guertner@physik.uni-muenchen.de\
+\n#$ -m bea\
+\n#$ -R y\
+\n#$ -l h_stack=200M\
+\n#$ -pe PE "+str(ncores)+"\
+\nexport OMP_NUM_THREADS="+str(ncores)+"\
+\nmodule load mathematica \n"
+if machine != "":
+    constant_part += "#$ -q " + machine + "\n"
+    
+
+
+#creating all folders
+iterator = [par[par["Iterator"]]]
+variable = [par["Iterator"].rstrip("_List")]
+#6th order check for FP in SE sampling
+if par["Froehlich_Polaron"] and par["Self_Energy"] and par["FP_6_ord_g0se"] and not par["SECumul"]:
+	iterator.append([6])
+	variable.append(["Max_Order"])
+
+#more than one iterator
+for mtoi in range(len(iterator)):
+	for i in iterator[mtoi]:
+		change_dir(variable[mtoi]+'_'+str(i))
+		current_loc = os.getcwd()
+	
+		if not os.path.exists(current_loc +"/ana"):
+			os.makedirs(current_loc +"/ana")
+		if not os.path.exists(current_loc +"/ana/control"):
+			os.makedirs(current_loc +"/ana/control")
+		if not os.path.exists(current_loc +"/data"):
+			os.makedirs(current_loc +"/data")
+		if not os.path.exists(current_loc +"/data/stats"):
+			os.makedirs(current_loc +"/data/stats")
+		if par["SECumul"]:
+			if not os.path.exists(current_loc +"/data/secumul"):
+				os.makedirs(current_loc +"/data/secumul")
+			
+		#copy exe and parameters
+		shutil.copy(build_loc + '/DiagMC_BEC', os.curdir)
+		shutil.copy(build_loc + '/DiagMC_BEC.json', os.curdir)
+		shutil.copy(build_loc + '/data_ana.py', os.curdir)
+		
+		#to Transform SE Sampling to data of FP
+		if par["Froehlich_Polaron"] and par["Self_Energy"]:
+			if not os.path.exists(current_loc +"/data/FP_control"):
+				os.makedirs(current_loc +"/data/FP_control")
+			shutil.copy(build_loc + '/FP_g0SE_Transform.m', os.curdir)
+			shutil.copy(build_loc + '/FP_g0SEg0_Transform.m', os.curdir)
+		
+		#update json file
+		par["Path"] = current_loc
+		par[variable[mtoi]]=i
+		jsonFile = open("DiagMC_BEC.json", "w+")
+		jsonFile.write(json.dumps(par))
+		jsonFile.close()
+			
+		#create .sge
+		filename = name + "_"+variable[mtoi]+"_" + str(i) + ".sge"
+		ofFile = open(filename, "w")
+		ofFile.write("#$ -cwd\n")
+		ofFile.write("#$ -o " + current_loc +'\n')
+		ofFile.write("#$ -e " + current_loc +'\n')
+		ofFile.write("#$ -N " + name + "_"+variable[mtoi]+"_" + str(i) + '\n')
+		ofFile.write(constant_part)
+		ofFile.write(current_loc + "/DiagMC_BEC\n")
+		ofFile.close()
+		
+		#submit
+		os.system("qsub " + filename)
+		os.chdir(os.pardir)
+	
+#compare code to FP
+if par["FP_Check"]:
+	#recompile for FP
+	os.chdir(build_loc+"/src")
+	with open('makefile', 'r') as makein:
+		maketext = makein.read().splitlines(True)
+	with open('makefile', 'w') as makeout:
+		makeout.write("DEFS = -DFP\n")
+		makeout.writelines(maketext[1:])
+	ncores = par['NCores']
+	os.system("export OMP_NUM_THREADS="+str(ncores))
+	os.system("make clean")
+	os.system("make")
+	
+	#create extra folder
+	os.chdir(execute_loc)
+	change_dir("FP_Check")
+	current_loc = os.getcwd()
+	if not os.path.exists(current_loc +"/ana"):
+		os.makedirs(current_loc +"/ana")
+	if not os.path.exists(current_loc +"/ana/control"):
+		os.makedirs(current_loc +"/ana/control")
+	if not os.path.exists(current_loc +"/data"):
+		os.makedirs(current_loc +"/data")
+	if not os.path.exists(current_loc +"/data/stats"):
+		os.makedirs(current_loc +"/data/stats")
+	shutil.copy(build_loc + '/DiagMC_BEC', os.curdir)
+	shutil.copy(build_loc + '/DiagMC_BEC.json', os.curdir)
+	shutil.copy(build_loc + '/data_ana.py', os.curdir)
+	
+	#update json file
+	par["Max_Order"] = -1
+	par["Momentum"] = 0.3
+	par["Chemical_Potential"] = -2.2
+	par["Alpha"] = 2
+	par["Froehlich_Polaron"] = True
+	par["BEC_Polaron"]= False
+	par["Self_Energy"]= False
+	par["SECumul"]= False
+	par["MEASREWEIGHT"]= False
+	par["FOG0SE"] = False
+	par["RunTime"] = 3600
+	par["Path"] = current_loc
+	jsonFile = open("DiagMC_BEC.json", "w+")
+	jsonFile.write(json.dumps(par))
+	jsonFile.close()
+	
+	#create .sge
+	filename = name + "_FP_Check.sge"
+	ofFile = open(filename, "w")
+	ofFile.write("#$ -cwd\n")
+	ofFile.write("#$ -o " + current_loc +'\n')
+	ofFile.write("#$ -e " + current_loc +'\n')
+	ofFile.write("#$ -N " + name + "_FP_Check\n")
+	ofFile.write(constant_part)
+	ofFile.write(current_loc + "/DiagMC_BEC\n")
+	ofFile.close()
+	
+	#submit
+	os.system("qsub " + filename)
+	os.chdir(os.pardir)
+
+
+#Check if 6 order of FP in SE Sampling still reproduces
+if par["Self_Energy"] and par["FP_6_ord_g0se"]:
+#recompile for FP
+	os.chdir(build_loc+"/src")
+	with open('makefile', 'r') as makein:
+		maketext = makein.read().splitlines(True)
+	with open('makefile', 'w') as makeout:
+		makeout.write("DEFS = -DFP " +options2 +"\n")
+		makeout.writelines(maketext[1:])
+	ncores = par['NCores']
+	os.system("export OMP_NUM_THREADS="+str(ncores))
+	os.system("make clean")
+	os.system("make")
+	
+	#create extra folder
+	os.chdir(execute_loc)
+	change_dir("FP_SE_6ord_Check")
+	current_loc = os.getcwd()
+	if not os.path.exists(current_loc +"/ana"):
+		os.makedirs(current_loc +"/ana")
+	if not os.path.exists(current_loc +"/ana/control"):
+		os.makedirs(current_loc +"/ana/control")
+	if not os.path.exists(current_loc +"/data"):
+		os.makedirs(current_loc +"/data")
+	if not os.path.exists(current_loc +"/data/stats"):
+		os.makedirs(current_loc +"/data/stats")
+		
+	if par["SECumul"]:
+		if not os.path.exists(current_loc +"/data/secumul"):
+			os.makedirs(current_loc +"/data/secumul")
+	shutil.copy(build_loc + '/DiagMC_BEC', os.curdir)
+	shutil.copy(build_loc + '/DiagMC_BEC.json', os.curdir)
+	shutil.copy(build_loc + '/data_ana.py', os.curdir)
+	
+	#to Transform SE Sampling to data of FP
+	if par["Froehlich_Polaron"] and par["Self_Energy"]:
+		if not os.path.exists(current_loc +"/data/FP_control"):
+			os.makedirs(current_loc +"/data/FP_control")
+		shutil.copy(build_loc + '/FP_g0SE_Transform.m', os.curdir)
+		shutil.copy(build_loc + '/FP_g0SEg0_Transform.m', os.curdir)
+	
+	#update json file
+	par["Max_Order"] = 6
+	par["Momentum"] = 0.3
+	par["Chemical_Potential"] = -2.2
+	par["Alpha"] = 2
+	par["Froehlich_Polaron"] = True
+	par["BEC_Polaron"]= False
+	par["SECumul"]= False
+	par["RunTime"] = 7200
+	par["Path"] = current_loc
+	jsonFile = open("DiagMC_BEC.json", "w+")
+	jsonFile.write(json.dumps(par))
+	jsonFile.close()
+	
+	#create .sge
+	filename = name + "_FP_SE_6ord.sge"
+	ofFile = open(filename, "w")
+	ofFile.write("#$ -cwd\n")
+	ofFile.write("#$ -o " + current_loc +'\n')
+	ofFile.write("#$ -e " + current_loc +'\n')
+	ofFile.write("#$ -N " + name + "_FP_SE_6ord\n")
+	ofFile.write(constant_part)
+	ofFile.write(current_loc + "/DiagMC_BEC\n")
+	ofFile.close()
+	
+	#submit
+	os.system("qsub " + filename)
+	os.chdir(os.pardir)
+		
+	
+
